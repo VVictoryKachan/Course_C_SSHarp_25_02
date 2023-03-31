6478f6c679a4fe2dbe7dd424ba122fdb0a61c32f
Victory Kachan <victory.kachan@gmail/com>
2023-03-31 21:54:52 +0300

    task_hw_4_3

---
+++ Lesson_4/HW_4/HW_4_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void Massiv(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+        Console.WriteLine();
+}
+int[] MassEightDigit(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassEightDigit(num, start, stop);
+Massiv(mass);
