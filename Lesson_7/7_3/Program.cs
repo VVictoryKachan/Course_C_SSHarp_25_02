18df7cfe2a9237d7286b7634d3780cf45ddd868a
Victory Kachan <victory.kachan@gmail/com>
2023-04-07 21:55:24 +0300

    change task_7_3

--- Lesson_7/7_3/Program.cs
+++ Lesson_7/7_3/Program.cs
@@ -1,6 +1,31 @@
 ﻿// 3. Задайте двумерный массив. Найдите сумму элементов главной диагонали.
 // https://4apple.org/summa-diagonalnyh-jelementov-matricy/
 
+void Print(int[,] arr)
+ {
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0; j<column; j++)
+        Console.Write($" {arr[i,j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+ }
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i=0; i<row; i++)
+        for (int j = 0; j<column; j++)
+        arr[i,j] = new Random().Next(from, to);
+
+    return arr;
+}
+
 int SumMass(int[,] array)
 {
     int sumarray = 0;
